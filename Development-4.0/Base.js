c017ba638fe5aafa26a87fb3bcca7c3d1e8d5500
ＮＯＩＺ Ξ <86784191+Zenoixnoize@users.noreply.github.com>
2022-02-27 23:25:21 +0530

    GARFIELD 4.0 BY NOIZE 🐼
    UPDATING TO 4.0 🐼

---
+++ Development-4.0/Base.js
@@ -0,0 +1,18 @@
+/* Copyright (C) 2022 - NOIZE
+*/
+
+'use strict';
+
+class Base {
+    constructor(client) {
+        Object.defineProperty(this, 'client', { value: client });
+    }
+
+    _clone() {
+        return Object.assign(Object.create(this), this);
+    }
+    
+    _patch(data) { return data; }
+}
+
+module.exports = Base;
---
+++ Development-4.0/index.js
@@ -0,0 +1,11 @@
+/* Copyright (C) 2022 - NOIZE
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
